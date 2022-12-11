0e7e59433da0cd19314632070659cf94bb1cc966
TaronMakaryan <taron.makaryan@mail.ru>
2022-12-11 06:52:39 -0800

    first commit

---
+++ to-do-list/src/App.js
@@ -0,0 +1,117 @@
+import React, { useState } from 'react'
+import './App.css';
+import './style.css'
+
+
+
+function App() {
+  const initialState = JSON.parse(localStorage.getItem("todos")) || []
+  const[allTodos,setTodos]=React.useState(initialState)
+  const[inputTodo,setTodo]=React.useState("")
+  const[inpStyle, setStyle] = useState("inp_ok");
+  const[warning54, setWarningState] = useState(false)
+  const[hideCompleted, setHideCompleted] = useState(false)
+
+
+  React.useEffect(()=>{
+    localStorage.setItem('todos',JSON.stringify(allTodos))
+  },[allTodos])
+
+  function inputChanged(event){
+    if (event.target.value.length > 54){
+      setStyle('inp_error')
+      setWarningState(true)
+    } else {
+      setStyle('inp_ok')
+      setWarningState(false)
+    }
+    setTodo(event.target.value)
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+
+    const newTodo = {
+      id:new Date().getTime(),
+      text:inputTodo,
+      completed:false
+    }
+
+    setTodos([newTodo, ...allTodos])
+    setTodo("")
+  }
+
+  function deleteTodo(id) {
+    
+    const updatedTodos = [...allTodos].filter((inputTodo)=>inputTodo.id !== id)
+
+    setTodos(updatedTodos)
+  }
+
+
+  function toggleComplete(id){
+    const updatedTodos=[...allTodos].map((todo) => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+    setTodos(updatedTodos)
+  }
+
+
+  function handleChange() {
+    const checkBox = document.querySelector('#inpcheck')
+
+    if (checkBox.checked) {
+      setHideCompleted(true)
+    } else {
+      setHideCompleted(false)
+    }
+  }
+  
+
+
+
+
+  return (
+
+    <div className="App">
+
+    
+
+
+      <div id="inp3">
+        <input type="checkbox" onChange={()=>handleChange()} value='checked' id='inpcheck' className='inputHide'/>
+        <h4>Hide Completed</h4>
+      </div>
+      <form onSubmit={handleSubmit} className="form1">
+        <h3 id="h31">Task</h3>
+        <input className={inpStyle} id='inputter' type="text" placeholder='Write here' onChange={inputChanged}  value={inputTodo}/>
+        <button className="btn1" type="submit" disabled={warning54}>Add Todo</button>
+        <label className={warning54 ? "label1" : "hideLabel"}>Task Contenet Can Contain Max 54 Characters.</label>
+      </form>    
+      {allTodos.map((todo)=><div key={todo.id}>
+        <div className='div1'> 
+          <table className='tbl1'>
+            <thead>
+              <tr className={todo.completed && hideCompleted ? "taskHidden": ""}>
+                <td>
+                  <input type="checkbox" id="inp2" onChange={()=> toggleComplete(todo.id)} checked={todo.completed}></input>
+                </td>
+                <td id='td1'>
+                  <h2 id="h222">{todo.text}</h2>
+                </td>
+                <td>
+                  <button id="btn2" onClick={()=> deleteTodo(todo.id)}>X</button>
+                </td>
+              </tr>
+            </thead>
+          </table>
+        </div>
+        </div>)}
+    </div>
+  );
+}
+
+export default App;
